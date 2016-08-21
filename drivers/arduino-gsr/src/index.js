515c8e81dab470815cb2a86f106dc139624fdfc7
elsehow <yes@cosmopol.is>
2016-08-21 15:53:56 -0700

    Passing in port to GSR now

--- drivers/arduino-gsr/src/index.js
+++ drivers/arduino-gsr/src/index.js
@@ -1,42 +1,16 @@
 var SerialPort = require('serialport');
-var empty = x => x.length==0
 var kefir = require('kefir')
-// calls back on (err, list-of-arduino-ports)
-function findArduinos (cb) {
-  SerialPort.list(function (err, ports) {
-    if (err) return console.log(err)
-    // console.log(ports)
-    var arduinoPorts = ports
-        .map(p => p.comName)
-        .filter(port => !port.search('/dev/cu.usbmodem'))
-    if (!empty(arduinoPorts))
-      return cb(null, arduinoPorts)
-    return cb('No arduinos found!', null)
-  })
-}
-// TODO try each port name in sequence?
-function choosePort (names) {
-  return names[0]
-}
 function open (portName, cb) {
   var port = new SerialPort(portName, {
     parser: SerialPort.parsers.readline(',\n')
-  }) 
+  })
   port.on('open', function (err) {
     cb(err, port)
   })
 }
 // returns a stream
-module.exports = function () {
+module.exports = function (portName) {
   return kefir
-    .fromNodeCallback(findArduinos)
-    .map(choosePort)
-    .flatMap(function (port) {
-      return kefir.fromNodeCallback(cb => {
-        return open(port, cb)
-      })
-    })
-    .flatMap(port => {
-      return kefir.fromEvents(port, 'data')
-    })
+    .fromNodeCallback(cb => open(portName, cb))
+    .flatMap(port => kefir.fromEvents(port, 'data'))
 }
--- drivers/arduino-gsr/test/index.js
+++ drivers/arduino-gsr/test/index.js
@@ -1,8 +1,24 @@
-var SerialPort = require('serialport');
 var gsr = require('..')
-var opts = {
-  port: '/dev/cu.usbmodem1421',
-}
-
-var sensor = gsr()
+var port = '/dev/cu.usbmodem1411'
+var sensor = gsr(port)
 sensor.log('yo')
+
+// var empty = x => x.length==0
+// var kefir = require('kefir')
+// // calls back on (err, list-of-arduino-ports)
+// function findArduinos (cb) {
+//   SerialPort.list(function (err, ports) {
+//     if (err) return console.log(err)
+//     // console.log(ports)
+//     var arduinoPorts = ports
+//         .map(p => p.comName)
+//         .filter(port => !port.search('/dev/cu.usbmodem'))
+//     if (!empty(arduinoPorts))
+//       return cb(null, arduinoPorts)
+//     return cb('No arduinos found!', null)
+//   })
+// }
+// // TODO try each port name in sequence?
+// function choosePort (names) {
+//   return names[0]
+// }
