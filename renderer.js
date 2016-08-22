c7e801fd01c736f6f59aa23d3d49fa6c3282f2d1
elsehow <yes@cosmopol.is>
2016-08-21 23:05:28 -0700

    IPC with main process; starting hyperlog

--- renderer.js
+++ renderer.js
@@ -1,5 +1,12 @@
-let setupScreen = require('./views/setup-screen')
-
+const {ipcRenderer} = require('electron')
+const setupScreen = require('./views/setup-screen')
+// the setup screen calls back on the user's configuration
 setupScreen(function (config) {
-  console.log('YOOOO!!', config)
+  // prepare to receive a reply - a stream of logged buffers
+  ipcRenderer.on('logged-buffer-stream', (event, stream) => {
+    // TODO Now we get a kefir stream of buffers written to the log
+    console.log('my stream', stream)
+  })
+  // send our config to the main process
+  ipcRenderer.send('config', config)
 })
